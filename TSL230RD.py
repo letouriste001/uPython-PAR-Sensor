4bf86fac2f553724c2ba280fb175ac7876de4d51
Christophe AUBERT <aubert.christophe.pro@gmail.com>
2016-10-18 11:37:31 +0200

    modification de la fonction mesure de watt/m2 en uwatt/cm2 ajoute de la fonction de moyenne de la valeur brute ajout de la fonction uwatt/cm2 moyenner ajout de la fonction uE/m2/s ajout de la fonction uE/m2/s moyenner ajout des constante a et b pour les valeur en uE

--- TSL230RD.py
+++ TSL230RD.py
@@ -1,5 +1,10 @@
 import pyb
 
+# Constantes
+FACOTR_A = 0.34
+FACOTR_B = -30.6
+
+# variables globales
 timer = 0
 t2 = None
 period = 1
@@ -17,20 +22,52 @@ def callback(line):
 
 
 # TODO fonction de moyenne pour ameliorer la valeur lue
+def raw_average(time_average=10):
+    for i in range(0, (time_average - 1)):
+        sum += raw_measure()
+        pyb.delay(1000)
+    try:
+        return sum / time_average
+    except ZeroDivisionError:
+        print("division by zero, time average is bad, please change value")
+        return -1
+
+
+#TODO commentaire
 def raw_measure():
     global period
     try:
         return 1000000 / period
     except ZeroDivisionError:
+        print("division by zero, period is null, please check your wiring")
         return -1
 
 
 # TODO fonction a verifier exactement
-def watt_m2_measure():
+def uwatt_by_cm2_measure():
     f0 = raw_measure()
     return ((f0 * 100) - 0.4) / 790
 
 
+# TODO fonction a verifier exactement
+def uwatt_by_cm2_average_measure(time_average=10):
+    f0 = raw_average(time_average)
+    return ((f0 * 100) - 0.4) / 790
+
+
+# TODO ameliorer la fonction et la commenter
+def Einstein_by_m2_by_second_measure():
+    f0 = raw_measure()
+    return ((f0 * FACOTR_A) + FACOTR_B)
+
+
+# TODO ameliorer la fonction et la commenter
+def Einstein_by_m2_by_second_average_measure(time_average=10):
+    f0 = raw_average(time_average)
+    return ((f0 * FACOTR_A) + FACOTR_B)
+
+
+#TODO Commenter
 def init(pin_name='X1', frequency_scalling=100):
     global t2
     global f0_scalling
